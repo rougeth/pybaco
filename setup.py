7637e22c8ead9ebbd754ca19c41a9f248862ec52
Marco Rougeth <marco@rougeth.com>
2015-01-19 10:18:48 -0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+import os
+
+
+setup(
+    name='pybaco',
+    description='Python Base Converter',
+    author='Marco Rougeth',
+    author_email='marco@rougeth.com',
+    url='http://github.com/rougeh/pybaco',
+    version='0.1',
+    py_modules=['baco'],
+)
